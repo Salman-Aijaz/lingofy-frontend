3dc5298e8b306784e7739aeaa9938e5393834ed0
Salman-Aijaz <salman333699@gmail.com>
2025-07-05 18:00:45 +0500

    pwa setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/logo-transparent.png" />
+        <meta name="theme-color" content="#000000" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
